a0e6fb88ccae3b22e75a3e050f2915558e5ff94f
Deweyfang <Deweyfang@gmail.com>
2023-11-08 22:08:13 -0800

    update debug code

--- process.py
+++ process.py
@@ -1,5 +1,10 @@
 import fileinput
 import sys
+import os
+
+cwd = os.getcwd()  # Get the current working directory (cwd)
+files = os.listdir(cwd)  # Get all the files in that directory
+print("Files in %r: %s" % (cwd, files))
 
 filelist = [fileinput.FileInput(f,inplace=True)for f in sys.argv[1].split('\n')]
 original = [line.strip() for line in sys.argv[2].split('\n')]
@@ -7,6 +12,7 @@ modified = [line.strip() for line in sys.argv[3].split('\n')]
 
 print(original)
 print(modified)
+print(filelist)
 
 replaced = False
 for file in filelist:
